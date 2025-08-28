94b0f8e62c75b899a203c9e1f43c29c853082ad4
Thiago Mazo <thiago.mazo@hotmail.com>
2025-08-28 12:34:22 -0300

    feat: remove unnecessary codes and create Proposals controller.

---
+++ src/Insurance.Plataform.Api/UseCases/Proposals/Create/ProposalsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Insurance.Plataform.Api.UseCases.Proposals.Create
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProposalsController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult Create()
+        {
+            return NoContent();
+        }
+    }
+}
