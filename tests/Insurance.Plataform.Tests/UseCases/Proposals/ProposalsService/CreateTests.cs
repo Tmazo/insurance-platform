9d4a229b58d23b0fad89d2d82a603fe02d3792f6
Thiago Mazo <thiago.mazo@hotmail.com>
2025-09-04 17:31:30 -0300

    test: create tests.

---
+++ tests/Insurance.Plataform.Tests/UseCases/Proposals/ProposalsService/CreateTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Insurance.Plataform.Application.Dtos;
+using Insurance.Plataform.Domain.Entities;
+using Insurance.Plataform.Domain.Enums;
+using NSubstitute;
+
+namespace Insurance.Plataform.Tests.UseCases.Proposals.ProposalsService;
+
+public class CreateTests() : TestBasedMock
+{
+    [Fact]
+    public async Task Create_Should_Success_Return_Id()
+    {
+        //Arrange
+        var createProposalRequest = new CreateProposalRequest("Test", EProposalStatus.Approved);
+        proposalRepository.AddAsync(Arg.Any<ProposalEntity>(), Arg.Any<CancellationToken>()).Returns(Guid.NewGuid());
+
+        //Act
+        var result = await proposalsService.CreateAsync(createProposalRequest, CancellationToken.None);
+
+        //Assert
+        result.Should().NotBeEmpty();
+    }
+}
---
+++ tests/Insurance.Plataform.Tests/UseCases/TestBasedMock.cs
@@ -0,0 +1,19 @@
+﻿using Insurance.Plataform.Application.UseCases.Proposals;
+using Insurance.Plataform.Domain.Repositories;
+using NSubstitute;
+
+namespace Insurance.Plataform.Tests.UseCases
+{
+    public class TestBasedMock
+    {
+        protected readonly IProposalsService proposalsService;
+        protected readonly IProposalRepository proposalRepository;
+
+        public TestBasedMock()
+        {
+            proposalRepository = Substitute.For<IProposalRepository>();
+
+            proposalsService = new ProposalsService(proposalRepository);
+        }
+    }
+}
